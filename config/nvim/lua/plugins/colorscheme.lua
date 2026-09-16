4ef6c0853c9e8203bfa15095e92471b06a2134dc
Nicolaj Hansen <git@nicolajbh.com>
2026-09-16 16:46:42 +0200

    remove old config files/

--- config/nvim/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{ "rebelot/kanagawa.nvim" },
-	{ "vague2k/vague.nvim" },
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {} },
-	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
-}
--- config/nvim/lua/plugins/lualine.lua
+++
@@ -1,5 +0,0 @@
-return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	opts = {},
-}
